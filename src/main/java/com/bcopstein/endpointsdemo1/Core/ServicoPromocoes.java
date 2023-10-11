79bce4b50da4cd7bd2298d27136eb009bc8d6bfd
Március Moraes Vargas <marcius.vargas@acad.pucrs.br>
2023-10-11 19:25:04 -0300

    ServicoPromocoes updated

--- src/main/java/com/bcopstein/endpointsdemo1/Core/ServicoPromocoes.java
+++ src/main/java/com/bcopstein/endpointsdemo1/Core/ServicoPromocoes.java
@@ -4,5 +4,17 @@ package com.bcopstein.endpointsdemo1.Core;
 //Factory
 
 public class ServicoPromocoes {
+    private double taxaPromocional;
+
+    public ServicoPromocoes(){
+        this.taxaPromocional= 0.05;
+    }
+
+    public ServicoPromocoes(double taxaPromocional){
+        this.taxaPromocional= taxaPromocional;
+    }
     
+    public String toString() {
+        return "Promoção: " + taxaPromocional;
+    }
 }
